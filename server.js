480ee7caaed78d70632fc828096410491ddd3759
SMakela <73409799+smakela13@users.noreply.github.com>
2023-09-02 01:07:17 -0400

    Updated server's error handling and added connection events and graceful shutdown for mongoose

--- server.js
+++ server.js
@@ -15,11 +15,54 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+// Mongoose connection
+const mongooseOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
+};
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', mongooseOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB:', err.message);
+  });
+
+// Mongoose connection events
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to the database.');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err.message}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected.');
+});
+
+// Function to close Mongoose connection gracefully
+function gracefulShutdown(msg, callback) {
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+}
+
+// For app termination
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error('Global error handler:', err.message);
+  res.status(err.status || 500).send({ error: err.message || 'An unexpected error occurred!' });
 });
 
 // routes
